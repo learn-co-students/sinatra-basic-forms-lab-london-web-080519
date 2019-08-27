9d69335013cefb3847fabc3a11bde105f2f8ae41
github username <github email address>
2019-08-27 16:47:37 +0100

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,9 @@
+class Puppy
+    def initialize(name, breed, months_old)
+        @name = name
+        @breed = breed
+        @months_old = months_old
+    end
+
+    attr_accessor :name, :breed, :months_old
+end
