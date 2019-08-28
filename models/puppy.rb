95f154ff3be525b7a4e7cc2491b0e04007448586
99chris99 <chrishodge87@gmail.com>
2019-08-28 07:31:35 +0100

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,18 @@
+class Puppy
+
+    attr_accessor :name, :months_old
+    attr_reader :breed
+    
+
+    def initialize (name, breed, months_old)
+        @name = name,
+        @breed = breed,
+        @months_old = months_old
+    end
+
+    def name
+        @name
+    end
+
+
+end#class end
